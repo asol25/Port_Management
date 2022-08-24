3635dd1c4b37e9534a7827610e8742ac74bd68a4
asol25 <usool.203@gmail.com>
2022-08-24 20:46:55 +0700

    add README.md file

--- src/Containers/HeavyContainer.java
+++ src/Containers/HeavyContainer.java
@@ -5,7 +5,6 @@ public class HeavyContainer extends Container {
     public HeavyContainer(int ID, int weight) {
         super(ID, weight);
     }
-
     @Override
     public double consumption() {
         return weight * 3.00;
--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,5 @@
 import java.io.*;
 
-
 public class Program {
     public static void main(String[] args) throws IOException {
         Process process = new Process();
